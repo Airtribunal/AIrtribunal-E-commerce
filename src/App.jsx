e81d78a7e7e6586227591eec5506c6498f360f19
Airtribunal <airtribunal@gmail.com>
2022-08-21 17:17:12 +0300

    build-folder

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ const App = () => {
       <section className="navbar">
         <Navbar />
       </section>
-      <div className="main">
+      <section className="main">
         <Main />
-      </div>
+      </section>
       <footer className="footer">
 
       </footer>
