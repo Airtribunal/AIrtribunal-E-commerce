e37489caa8e94acb44c5adced08069df6089ce9a
Airtribunal <airtribunal@gmail.com>
2022-08-21 17:30:06 +0300

    card-remaking

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     return (
         <div className="nav">
             <div className="nav-container">
-                <Link to="/"><h1 className="logo">Airtribunal</h1></Link>
+                <Link to="/" onClick={() => window.location.replace("#")}><h1 className="logo">Airtribunal</h1></Link>
                 <box-icon id="cart" className="cart-icon" name='shopping-bag'></box-icon>
             </div>
             <div />
